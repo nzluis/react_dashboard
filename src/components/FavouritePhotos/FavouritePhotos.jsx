dedf24045a8d8200a34022736b2286aef346828a
luis <luisnavarroFS@gmail.com>
2024-02-19 20:34:24 +0100

    feat: favourites handleClick and key by id

--- src/components/FavouritePhotos/FavouritePhotos.jsx
+++ src/components/FavouritePhotos/FavouritePhotos.jsx
@@ -1,12 +1,17 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { removePhoto } from "../../features/favouritesSlice"
 
 export const FavouritePhotos = () => {
     const favourites = useSelector((state) => state.favourites)
+    const dispatch = useDispatch()
+    function handleClick(e) {
+        dispatch(removePhoto(e.target.alt))
+    }
     return (
         <>
             <h1>Favourites</h1>
-            {favourites && favourites.map((favouritePic, index) => {
-                return <img key={index} src={favouritePic.src} alt="" />
+            {favourites && favourites.map((favouritePic) => {
+                return <img key={favouritePic.id} onClick={(e) => handleClick(e)} src={favouritePic.src} alt={favouritePic.id} />
             })}
         </>
     )
