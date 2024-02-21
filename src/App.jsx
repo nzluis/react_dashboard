9248562746963d372e9e6a142b2e8613e78fb5f6
luis <luisnavarroFS@gmail.com>
2024-02-21 18:53:51 +0100

    feat: navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { NavBar } from "./components/NavBar/NavBar"
+import { Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import { AllPhotos } from "./pages/AllPhotos/AllPhotos"
 import { FavouritePhotos } from "./pages/FavouritePhotos/FavouritePhotos"
+import { NavBar } from './components/NavBar/NavBar'
+
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<NavBar />}>
+    <Route path='/' element={<AllPhotos />} />
+    <Route path='/myphotos' element={<FavouritePhotos />} />
+  </Route>
+));
 
 function App() {
   return (
     <>
-      <NavBar />
-      <AllPhotos />
-      <FavouritePhotos />
+      <RouterProvider router={appRouter} />
     </>
   )
 }
 
 export default App
+
+// export default function App() {
+//   return (
+//     <>
+//       <NavBar />
+//       <AllPhotos />
+//       <FavouritePhotos />
+//     </>
+//   )
+// }
+
