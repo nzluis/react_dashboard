0aebd824484d762ef43360c1fed6dd9ff81a0214
luis <luisnavarroFS@gmail.com>
2024-02-23 12:28:51 +0100

    refactor: clean code

--- src/pages/AllPhotos/AllPhotos.jsx
+++ src/pages/AllPhotos/AllPhotos.jsx
@@ -24,7 +24,6 @@ export const AllPhotos = () => {
         window.scrollTo(0, 0)
     }, [pathname])
 
-
     useEffect(() => {
         if (term !== '') {
             if (status === 'idle') {
@@ -45,10 +44,7 @@ export const AllPhotos = () => {
         } else {
             alert(`Sorry, there was an error: ${error}`)
         }
-
-
     }, [dispatch, allPhotos, status, term, error])
-
 
     function handleLike(pic) {
         dispatch(addPhoto({
@@ -64,30 +60,10 @@ export const AllPhotos = () => {
             created_at: new Date(Date.now()).toLocaleDateString("es-ES"),
         }))
     }
+
     return (
         <>
             <Header />
-            {/* {isDesktopOrLaptop && <div className={styles.container}>
-                <div className={styles.first_column}>
-                    <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.icon}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                        </svg>
-                        <input
-                            placeholder="LOOK FOR A PICTURE..."
-                            className={styles.input}
-                            value={searchInput}
-                            onChange={(e) => { setSearchInput(e.target.value) }}
-                        />
-                        <button style={{ display: 'none' }} type="submit"></button>
-                    </form>
-                    <h1 className={styles.h1}>FIND YOUR VISION</h1>
-                </div>
-                <div className={styles.second_column}>
-                    <h2 className={styles.h2}>BEST FREE PICTURES ON INTERNET</h2>
-                    <h3 className={styles.h3}>All kind of images shared by their creators</h3>
-                </div>
-            </div>} */}
             <div className={styles.cards_container}>
                 {!isLoading && allPhotos ? allPhotos.map((pic) => {
                     return (
--- src/pages/FavouritePhotos/FavouritePhotos.jsx
+++ src/pages/FavouritePhotos/FavouritePhotos.jsx
@@ -4,11 +4,8 @@ import { Select, MenuItem, InputLabel, FormControl } from "@mui/material"
 import InfoIcon from '@mui/icons-material/Info';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import styles from './favouritesPhotos.module.css'
-
-
 import Zoom from 'react-medium-image-zoom'
 import 'react-medium-image-zoom/dist/styles.css'
-
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { ModalComponent } from "../../components/Modal/ModalComponent";
@@ -27,14 +24,10 @@ export const FavouritePhotos = () => {
 
     const favourites = useSelector((state) => state.favourites.photos)
     const term = useSelector((state) => state.favourites.term)
-    const dispatch = useDispatch()
-
     const [selectedPic, setSelectedPic] = useState({})
     const [orderBy, setOrderBy] = useState('')
     const [open, setOpen] = useState(false);
-
-
-
+    const dispatch = useDispatch()
     const filterBySearch = getOrderedPhotos(getFilteredPhotos(favourites, term), orderBy)
     const { pathname } = useLocation()
 
@@ -47,40 +40,14 @@ export const FavouritePhotos = () => {
         localStorage.setItem('favouritePhotos', JSON.stringify(favourites))
     }, [favourites])
 
-
-
     function handleModal(photo) {
         setSelectedPic(photo)
         setOpen(true)
     }
 
-
-
     return (
         <>
             <Header />
-            {/* {isDesktopOrLaptop && <div className={styles.container}>
-                <div className={styles.first_column}>
-                    <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.icon}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                        </svg>
-                        <input
-                            placeholder="SEARCH ON YOUR PHOTOS..."
-                            className={styles.input}
-                            value={searchInput}
-                            onChange={(e) => { setSearchInput(e.target.value) }}
-                            onKeyDown={(e) => e.key === 'Enter' && dispatch(addMyPhotosTerm(searchInput)) && setSearchInput('')}
-                        />
-                        <button style={{ display: 'none' }} type="submit"></button>
-                    </form>
-                    <h1 className={styles.h1}>FIND YOUR VISION</h1>
-                </div>
-                <div className={styles.second_column}>
-                    <h2 className={styles.h2}>YOUR LOCAL PHOTO STORAGE</h2>
-                    <h3 className={styles.h3}>Manage all your favourite photos as you wish</h3>
-                </div>
-            </div>} */}
             <div className={styles.cards_container}>
                 <FormControl fullWidth >
                     <InputLabel className={styles.select} id="orderby">Order by</InputLabel>
